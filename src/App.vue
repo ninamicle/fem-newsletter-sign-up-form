457ccb7caeb2b3ba3eec5a153642da3d356cd9a7
Nina <nina.micleusanu93@gmail.com>
2024-05-06 22:39:53 +0200

    desktop layout wip

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Form from "./components/Form.vue";
+import desktopImg  from "/public/illustration-sign-up-desktop.svg";
+import mobileImg  from "/public/illustration-sign-up-mobile.svg";
+import successIcon  from "/public/icon-success.svg";
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 items-center bg-white rounded-[1.9rem] pl-16 pr-6 py-6">
+    <div class="h-full my-20">
+      <div class="flex flex-col pt-4 ">
+        <h1 class="font-bold text-[3.4rem] text-darkSlateGrey tracking-wide">Stay updated!</h1>
+        <p class="text-base">
+          Join 60.000+ product managers receiving monthly updates on:
+        </p>
+        <div class="flex gap-4">
+          <img class="w-5 h-5" v-bind:src="successIcon" alt="Success Icon">
+          <p>Product discovery and building what matters</p>
+        </div>
+        <div class="flex gap-4">
+          <img class="w-5 h-5" v-bind:src="successIcon" alt="Success Icon">
+          <p>Measuring to ensure updates are a success</p>
+        </div>
+        <div class="flex gap-4">
+          <img class="w-5 h-5" v-bind:src="successIcon" alt="Success Icon">
+          <p>And much more!</p>
+        </div>
+      </div>
+      <Form></Form>
+    </div>
+    <div class="flex justify-end">
+      <img v-bind:src="desktopImg" alt="Sign up to our newsletter"/>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
---
+++ src/components/Form.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from "vue";
+
+const email = ref("")
+const handleSubmit = ()=>{
+console.log(email.value);
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit" class="flex flex-col gap-y-4">
+    <div class="flex flex-col">
+      <label class="font-bold py-2" for="email">Email address</label>
+      <input class="border border-grey rounded-lg h-12 placeholder:px-6"
+             v-model="email" type="email" id="email"
+             placeholder="email@company.com"/>
+    </div>
+    <button class="bg-darkSlateGrey text-white font-bold tracking-wide h-12 rounded-lg w-full" type="submit">Subscribe to monthly newsletter</button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
